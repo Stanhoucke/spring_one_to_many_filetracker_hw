3649f2f4d61d2957d727daf33a5379c6a0451ac6
Stanhoucke <stanhoucke@gmail.com>
2021-02-23 16:18:37 +0000

    initial commit

---
+++ src/test/java/com/codeclan/example/filetracker/FiletrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiletrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

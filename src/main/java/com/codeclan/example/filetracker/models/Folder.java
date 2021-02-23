f8b3646d6b68c18a98c02746e1640a12a33248a6
Stanhoucke <stanhoucke@gmail.com>
2021-02-23 17:42:23 +0000

    file and folder class

--- src/main/java/com/codeclan/example/filetracker/models/Folder.java
+++ src/main/java/com/codeclan/example/filetracker/models/Folder.java
@@ -1,4 +1,74 @@
 package com.codeclan.example.filetracker.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonIgnoreProperties({"file"})
+    @OneToMany(mappedBy = "file")
+    private List<File> files;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"folders"})
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.files = new ArrayList<File>();
+        this.user = user;
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void addFile(File file) {
+        this.files.add(file);
+    }
 }
